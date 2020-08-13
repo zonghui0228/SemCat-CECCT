beafff7282d1c05815b468841e2b6e27c9b096ac
zonghui0228 <1632755@tongji.edu.cn>
2020-08-13 11:13:49 +0800

    update

--- README.md
+++ README.md
@@ -1,1 +1,43 @@
 # SemCat-CECCT
+> Semantic categorization of Chinese eligibility criteria in clinical trials using machine learning methods.
+
+## data collection
+
+We downloaded the clinical trials registration files from website of Chinese Clinical Trials Registry ([ChiCTR](http://www.chictr.org.cn/)), and extracted the eligibility criteria text, including Chinese inclusion criteria(CIC), English inclusion criteria(EIC), Chinese exclusion criteria(CEC) and English exclusion criteria(EEC). An eligibility criteria example (registration number: [ChiCTR1800016069](http://www.chictr.org.cn/showproj.aspx?proj=27068)) shows below:
+
+| **Chinese inclusion criteria**                               | **English inclusion criteria**                               |
+| ------------------------------------------------------------ | ------------------------------------------------------------ |
+| 1）首次于本中心行肝癌切除术，术后组织病理学证实HCC；<br/>2）术前血清HBsAg（+）；<br/>3）血清HBV-DNA低于检测下限（内标法）；<br/>4）肿瘤未侵犯门静脉、肝静脉或胆管的主要分支；<br/>5）肝功能Child-Pugh A或B级；<br/>6）年龄18～65岁，性别不限；<br/>7）单发肿瘤者，最大径≤5cm；多发肿瘤者，瘤体数≤3个且各瘤体最大径≤3cm； | 1. patient receives hepatectomy of HCC, which was confirmed with pathology;<br/>2. serum HBsAg（+）is confirmed preoperatively;<br/>3. serum HBV-DNA is lower than minimum of detection;<br/>4. tumor did not invade potal vein,hepatic vein or major branch of biliary tract;<br/>5. liver function with Child-Pugh A or B;<br/>6. aged from 18-65 years male or female;<br/>7. single tumor≤5cm.for multiple tumors,number of tumor≤3 and every tumor≤3cm. |
+| **Chinese exclusion criteria**                               | **English exclusion criteria**                               |
+| 1）肝细胞癌合并肝外转移；<br/>2) 近1年内有抗病毒治疗；<br/>3）合并其他部位恶性肿瘤，或其他器官功能衰竭；<br/>4）合并其它肝炎病毒重叠感染；<br/>5）术前接受TACE或其他抗肿瘤治疗；<br/>6）肝功能Child-Pugh C级<br/>7）存在顽固性腹水或肝性脑病；<br/>8）因精神病、心理性疾病或其他原因不能配合治疗或不能完成疗程者。 | 1. HCC with metastasis out of the liver;<br/>2. antiviral therapy to HBV within one year;<br/>3. with other malignancy or organ failure;<br/>4. combined with other hepatic virus infection;<br/>5. recieved TACE or other antitumor therapy;<br/>6. liver function with Child-Pugh C;<br/>7. refractory Ascites or hepatic encephalopathy;<br/>8. patient who can not cooperate due to mental diseases or other reasons. |
+
+The eligibility criteria text is organized with multiple sentences. After sentence segmentation and filter, we randomly selected 19185 sentences used in the unsupervised hierarchical clustering section for criteria categories induction, and 38341 sentences used in supervised criteria classification section for the classification capacity validation of induced semantic categories.
+
+## clustering
+We used the agglomerative hierarchical clustering algorithm, works in a “bottom-up” manner, to cluster constructed semantic feature matrix and generated clusters based on criteria sentences similarity. The package ‘scikit-learn’ of Python was applied for the clustering section.
+
+1. get 19185 Chinese-English eligibility criteria sentences pairs.
+2. get UMLS semantic types of English eligibility criteria sentences using MetaMap.
+3. convert criteria sentences to feature matrix based on UMLS semantic types.
+4. hierarchical clustering and semantic categories induction.
+
+## classification
+
+To assess the classification capacity of our induced semantic categories used for the Chinese eligibility criteria sentences classification, we randomly selected 38341 Chinese eligibility criteria sentences and implemented 9 classification algorithms.
+
+- Machine learning algorithms
+  - NB
+  - kNN
+  - LR
+  - SVM
+- Deep learning algorithms
+  - CNN
+  - RNN
+  - FastText
+- Pre-trained language models
+  - BERT
+  - ERNIE
+
+## Contacts
+Zong Hui, Tongji University, Shanghai, 200092, China, [zonghui@tongji.edu.cn](mailto:zonghui@tongji.edu.cn)
+
---
+++ classification/CNN/CNN.py
@@ -0,0 +1,272 @@
+# 使用文本卷积网络模型，分类中文临床试验筛选标准Criteria
+# 类别有44类
+# 包括模型训练，模型保存，测试集评估
+
+
+# 引入要用到的库和定义全局变量
+from __future__ import print_function
+import os
+import sys
+import math
+import codecs
+import numpy as np
+
+import paddle
+import paddle.fluid as fluid
+
+
+
+# 文本卷积神经网络模型结构
+def convolution_net(data, input_dim, class_dim, emb_dim, hid_dim):
+    emb = fluid.embedding(
+        input=data, size=[input_dim, emb_dim], is_sparse=True)
+    conv_3 = fluid.nets.sequence_conv_pool(
+        input=emb,
+        num_filters=hid_dim,
+        filter_size=3,
+        act="tanh",
+        pool_type="sqrt")
+    conv_4 = fluid.nets.sequence_conv_pool(
+        input=emb,
+        num_filters=hid_dim,
+        filter_size=4,
+        act="tanh",
+        pool_type="sqrt")
+    conv_5 = fluid.nets.sequence_conv_pool(
+        input=emb,
+        num_filters=hid_dim,
+        filter_size=5,
+        act="tanh",
+        pool_type="sqrt")
+    prediction = fluid.layers.fc(input=[conv_3, conv_4, conv_5], size=class_dim, act="softmax")
+    return prediction
+
+
+# 定义预测程序（inference_program）。预测程序使用convolution_net来对fluid.layer.data的输入进行预测
+def inference_program(word_dict):
+    data = fluid.data(name="words", shape=[None], dtype="int64", lod_level=1)
+    dict_dim = len(word_dict)
+    net = convolution_net(data, dict_dim, CLASS_DIM, EMB_DIM, HID_DIM)
+    return net
+
+
+# 定义了training_program。它使用了从inference_program返回的结果来计算误差。。
+def train_program(prediction):
+    label = fluid.data(name="label", shape=[None, 1], dtype="int64")
+    cost = fluid.layers.cross_entropy(input=prediction, label=label)
+    avg_cost = fluid.layers.mean(cost)
+    accuracy = fluid.layers.accuracy(input=prediction, label=label)
+    return [avg_cost, accuracy]   #返回平均cost和准确率acc
+
+# 定义优化函数optimizer_func
+def optimizer_func(lr):
+    return fluid.optimizer.Adagrad(learning_rate=lr)
+
+
+# 定义数据提供器
+def reader_creator(data, word_dict, tag_dict):
+    UNK = word_dict['<unk>']
+    INS = []
+
+    with codecs.open(data, "r", encoding="utf-8") as f:
+        for line in f:
+            l = line.strip().split("\t")
+            INS.append(([word_dict.get(w, UNK) for w in l[2]], tag_dict[l[1]]))
+
+    def reader():
+        for doc, label in INS:
+            yield doc, label
+    return reader
+
+# 该函数用来计算训练中模型在test数据集上的结果
+def train_test(program, reader):
+    count = 0
+    feed_var_list = [program.global_block().var(var_name) for var_name in feed_order]
+    feeder_test = fluid.DataFeeder(feed_list=feed_var_list, place=place)
+    test_exe = fluid.Executor(place)
+    accumulated = len([avg_cost, accuracy]) * [0]
+    for test_data in reader():
+        avg_cost_np = test_exe.run(
+            program=program,
+            feed=feeder_test.feed(test_data),
+            fetch_list=[avg_cost, accuracy])
+        accumulated = [x[0] + x[1][0] for x in zip(accumulated, avg_cost_np)]
+        count += 1
+    return [x / count for x in accumulated]
+
+
+
+if __name__ == "__main__":
+
+    CLASS_DIM = 44    # 情感分类的类别数
+    EMB_DIM = 128     # 词向量的维度
+    HID_DIM = 512     # 隐藏层的维度
+    BATCH_SIZE = 256  # batch的大小
+    lr = 0.002        # 学习率
+
+    train_data = "./data/train_data.txt"                         # 训练数据
+    test_data = "./data/test_data.txt"                           # 测试数据
+    test_data_predict = "./data/test_data_predict.txt"           # 测试数据的预测结果，用于模型最终评估
+    dict_data = './data/dict.txt'                                # 字典数据
+    tag_data = './data/tags.txt'                                 # 类别
+    params_dirname = "eligibilitycriteria_conv.inference.model"  # 保存模型。参数
+
+    train = False   # 控制是否进行训练
+    predict = True # 进行预测
+    evaluate = False # 在测试集上进行评估
+
+    # 载入字典
+    print("Loading Eligibility Criteria word dict......")
+    with open(dict_data, 'r', encoding='utf-8') as f_dict:
+        word_dict = eval(f_dict.readlines()[0])
+    print("word dict length：{}".format(len(word_dict)))
+
+    # 载入类别
+    print("Loading Eligibility Criteria category tags......")
+    with open(tag_data, 'r', encoding='utf-8') as f_tag:
+        tag_dict = eval(f_tag.readlines()[0])
+    print("category number：{}".format(len(tag_dict)))
+
+    # 载入训练数据
+    print ("Reading training data......")
+    train_reader = fluid.io.batch(fluid.io.shuffle(reader_creator(train_data, word_dict, tag_dict), buf_size=25000), batch_size=BATCH_SIZE)
+
+    # 载入测试数据
+    print ("Reading testing  data......")
+    test_reader = fluid.io.batch(reader_creator(test_data, word_dict, tag_dict), batch_size=BATCH_SIZE)
+
+    # 进行训练
+    if train == True:
+        # 选择CPU进行训练
+        use_cuda = True  #在cpu上进行训练
+        place = fluid.CUDAPlace(0) if use_cuda else fluid.CPUPlace()
+
+        # 构造训练器, 训练器需要一个训练程序和一个训练优化函数。
+        exe = fluid.Executor(place)
+        prediction = inference_program(word_dict)
+        [avg_cost, accuracy] = train_program(prediction) #训练程序
+        sgd_optimizer = optimizer_func(lr) #训练优化函数
+        sgd_optimizer.minimize(avg_cost)
+
+        # 提供数据并构建主训练循环
+        # feed_order用来定义每条产生的数据和fluid.layers.data之间的映射关系。比如，imdb.train产生的第一列的数据对应的是words这个特征。
+        feed_order = ['words', 'label']
+        pass_num = 30  #训练循环的轮数
+
+        #程序主循环部分
+        def train_loop():
+            #启动上文构建的训练器
+            exe.run(fluid.default_startup_program()) # 进行参数初始化
+
+            feed_var_list_loop = [fluid.default_main_program().global_block().var(var_name) for var_name in feed_order]
+            feeder = fluid.DataFeeder(feed_list=feed_var_list_loop, place=place)
+            
+            # 获取预测程序
+            test_program = fluid.default_main_program().clone(for_test=True)
+
+            #训练循环
+            for epoch_id in range(pass_num):
+                for step_id, data in enumerate(train_reader()):
+                    #运行训练器  
+                    metrics = exe.run(fluid.default_main_program(), feed=feeder.feed(data), fetch_list=[avg_cost, accuracy])
+
+                    #在测试集上测试结果
+                    avg_cost_test, acc_test = train_test(test_program, test_reader)
+                    print('Step {0}, Test Loss {1:0.2}, Acc {2:0.2}'.format(step_id, avg_cost_test, acc_test)) # 测试集上的评估评估
+                    print("Step {0}, Epoch {1} Metrics {2}".format(step_id, epoch_id, list(map(np.array,metrics)))) # 训练集上的评估评估
+
+            if params_dirname is not None:
+                fluid.io.save_inference_model(params_dirname, ["words"], prediction, exe)#保存模型
+            return
+        # 开始训练
+        train_loop()
+        
+
+    # 进行预测
+    if predict == True:
+        # 选择CPU进行预测
+        use_cuda = False  #在cpu上进行预测
+        place = fluid.CUDAPlace(0) if use_cuda else fluid.CPUPlace()
+        
+        # 单条测试用输入数据
+        criteria_str = [u'年龄大于18岁', u'性别不限', u'过去经常酗酒']
+        criterias = [[w for w in c] for c in criteria_str]
+
+        # 构建预测器
+        exe = fluid.Executor(place)
+        inference_scope = fluid.core.Scope()
+
+        UNK = word_dict['<unk>']
+        lod = []
+        for c in criterias:
+            lod.append([word_dict.get(words, UNK) for words in c])
+
+        base_shape = [[len(c) for c in lod]]
+        lod = np.array(sum(lod, []), dtype=np.int64)
+
+        tensor_words = fluid.create_lod_tensor(lod, base_shape, place)
+        
+        # 应用模型并进行预测
+        with fluid.scope_guard(inference_scope):
+            [inferencer, feed_target_names, fetch_targets] = fluid.io.load_inference_model(params_dirname, exe)
+
+            assert feed_target_names[0] == "words"
+            results = exe.run(inferencer,
+                              feed={feed_target_names[0]: tensor_words},
+                              fetch_list=fetch_targets,
+                              return_numpy=False)
+            np_data = np.array(results[0])
+            for i, probability in enumerate(np_data):
+                prob = probability.tolist()
+                category = list(tag_dict.keys())[prob.index(max(prob))]
+                print("the maximum predict probability of category for eligibility criteria sentence: [{}] is [{}]".format(criteria_str[i], category))
+
+
+    # 在测试集上进行评估
+    if evaluate == True:
+        # 读取测试数据
+        # 选择CPU进行预测
+        use_cuda = False  #在cpu上进行预测
+        place = fluid.CUDAPlace(0) if use_cuda else fluid.CPUPlace()
+
+        # 读取测试数据，生成预测结果，用于评估
+        with codecs.open(test_data, "r", encoding="utf-8") as f:
+            criteria_data = [line.strip().split("\t") for line in f]
+        criteria_str = [d[2] for d in criteria_data]
+        criterias = [[w for w in c] for c in criteria_str]
+
+        # 构建预测器
+        exe = fluid.Executor(place)
+        inference_scope = fluid.core.Scope()
+
+        UNK = word_dict['<unk>']
+        lod = []
+        for c in criterias:
+            lod.append([word_dict.get(words, UNK) for words in c])
+
+        base_shape = [[len(c) for c in lod]]
+        lod = np.array(sum(lod, []), dtype=np.int64)
+
+        tensor_words = fluid.create_lod_tensor(lod, base_shape, place)
+        
+        # 应用模型并进行预测
+        predict_category = []
+        with fluid.scope_guard(inference_scope):
+            [inferencer, feed_target_names, fetch_targets] = fluid.io.load_inference_model(params_dirname, exe)
+
+            assert feed_target_names[0] == "words"
+            results = exe.run(inferencer,
+                              feed={feed_target_names[0]: tensor_words},
+                              fetch_list=fetch_targets,
+                              return_numpy=False)
+            np_data = np.array(results[0])
+            for i, probability in enumerate(np_data):
+                prob = probability.tolist()
+                category = list(tag_dict.keys())[prob.index(max(prob))]
+                predict_category.append(category)
+        with codecs.open(test_data_predict, "w", encoding="utf-8") as outf:
+            for i in range(len(predict_category)):
+                outf.write("{}\t{}\t{}\r\n".format(criteria_data[i][0], predict_category[i], criteria_data[i][2]))
+        
+        # 终端输入命令进行评估：
+        # python evaluation.py ./data/test_data.txt ./data/test_data_predict.txt > ./data/test_data_evaluation.txt
